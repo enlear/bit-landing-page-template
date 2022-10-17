4e0cad0ff90f7a67974ad2f08d61b7998ca44d08
Lakindu Hewawasam <lakindu2002@live.com>
2022-10-17 12:18:21 +0530

    updated export on footer

--- bit-landing-page-template/ui/footer/index.ts
+++ bit-landing-page-template/ui/footer/index.ts
@@ -1,6 +1,3 @@
 export { Footer } from './footer';
 export type { FooterProps } from './footer';
 export type { FooterBrand, FooterItem, FooterKind, FooterLink } from './footer.types';
-import { FooterBrandLabels } from './footer.types';
-
-export { FooterBrandLabels };
