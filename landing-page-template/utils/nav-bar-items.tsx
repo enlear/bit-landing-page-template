4aabc64d0a6f53ded9b8250562bededd13a88d70
Lakindu Hewawasam <lakindu2002@live.com>
2022-10-18 13:48:36 +0530

    updated home page

---
+++ landing-page-template/utils/nav-bar-items.tsx
@@ -0,0 +1,70 @@
+import { FooterItem } from "@enlear/bit-landing-page-template.ui.footer";
+import { MenuItem } from "@enlear/bit-landing-page-template.ui.header";
+import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
+import FacebookOutlinedIcon from "@mui/icons-material/FacebookOutlined";
+import { Avatar, alpha } from "@mui/material";
+import InfoIcon from "@mui/icons-material/Info";
+
+const headerItems: MenuItem[] = [
+  {
+    label: "Company",
+    expandable: true,
+    color: "primary",
+    sub: [
+      {
+        label: "About",
+        color: "info",
+        description: "Learn more about us",
+        href: "/about",
+        icon: <InfoOutlinedIcon />,
+      },
+    ],
+  },
+];
+const footerItems: FooterItem[] = [
+  {
+    kind: "brand",
+    title: "Volt",
+    links: [
+      {
+        title: "Facebook",
+        url: "https://www.facebook.com",
+        icon: (
+          <Avatar
+            variant="rounded"
+            sx={{
+              background: (theme) => alpha(theme.palette.info.main, 0.08),
+              color: "info.main",
+              ":hover": {
+                background: (theme) => alpha(theme.palette.info.main, 0.1),
+              },
+            }}
+          >
+            <FacebookOutlinedIcon />
+          </Avatar>
+        ),
+      },
+    ],
+  },
+  {
+    kind: "links",
+    title: "Company",
+    links: [
+      {
+        title: "About",
+        url: "/about",
+        icon: <InfoIcon />,
+      },
+      {
+        title: "Contact",
+        url: "/contact",
+        icon: <InfoIcon />,
+      },
+    ],
+  },
+];
+
+export const navBarItems = {
+  headerItems,
+  footerItems,
+};
